822906981129678698d6b5cf3f6212f1281e2835
Stephanie Wang <scbqwang@gmail.com>
2021-08-10 15:16:11 -0400

    connect user auth frontend

--- frontend/project/scripts/main.js
+++ frontend/project/scripts/main.js
@@ -3,6 +3,8 @@ var signUp = document.getElementById("sign-up");
 var signForm = document.getElementById("sign-up-form")
 var logIn = document.getElementById("log-in")
 var logForm = document.getElementById("log-in-form")
+var sMsg = document.getElementsByClassName("message")[0];
+var lMsg = document.getElementsByClassName("message")[1];
 var span = document.getElementsByClassName("close")[0];
 
 // on sign up click, show modal, open sign up form and hide log in form 
@@ -22,16 +24,36 @@ logIn.onclick = function () {
 // close modal on user click of [x]
 span.onclick = function () {
   modal.style.display = "none";
+  if (sMsg) {
+    sMsg.remove();
+    lMsg.remove();
+  }
 }
 
 // close modal on user click outside the area
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    if (sMsg) {
+      sMsg.remove();
+      lMsg.remove();
+    }
   }
 }
 
 // close modal if form is submitted
 function submitForm() {
   modal.style.display = "none";
+}
+
+function checkModals(modalNum) {
+  if (modalNum === 1) {
+    modal.style.display = "block";
+    signForm.style.display = "block";
+    logForm.style.display = "none"; 
+  } else if (modalNum === 2) {
+    modal.style.display = "block";
+    signForm.style.display = "none";
+    logForm.style.display = "block"; 
+  }
 }
