99f6169a024b73ba74c307be7c218ece711ce36d
Ciara (CiCi) Swann <cccswann@gmail.com>
2021-08-18 11:22:32 -0700

    Update __init__.py

--- backend/test/__init__.py
+++ backend/test/__init__.py
@@ -1,12 +1,6 @@
 import pytest
 
 from app import app
-"""Initialize the testing environment
-
-Creates an app for testing that has the configuration flag ``TESTING`` set to
-``True``.
-
-"""
 
 @pytest.fixture
 def client():
