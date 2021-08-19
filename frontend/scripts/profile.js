6fafaa01d516b5cb0647155a660e28ead7cb5036
Lance Ellis <scdelance@gmail.com>
2021-08-19 13:52:55 -0500

    profile pictures omg

--- frontend/scripts/profile.js
+++ frontend/scripts/profile.js
@@ -1,14 +1,14 @@
-var postButton = document.getElementById("new-post-button");
-var newPostModal = document.getElementById("new-post-modal");
-var newPostForm = document.getElementById("new-post");
-var imageSrc = document.getElementById("cp-image-src");
-var uploadedImage = document.getElementById("cp-image-uploaded");
+var postButton = document.getElementById("new-post-button")
+var newPostModal = document.getElementById("new-post-modal")
+var newPostForm = document.getElementById("new-post")
+var imageSrc = document.getElementById("cp-image-src")
+var uploadedImage = document.getElementById("cp-image-uploaded")
 
-var followModal = document.getElementById("follow-modal");
-var followers = document.getElementById("followers");
-var following = document.getElementById("following");
-var followDiv = document.getElementById("following-div");
-var followerDiv = document.getElementById("followers-div");
+var followModal = document.getElementById("follow-modal")
+var followers = document.getElementById("followers")
+var following = document.getElementById("following")
+var followDiv = document.getElementById("following-div")
+var followerDiv = document.getElementById("followers-div")
 
 if (postButton) {
   postButton.onclick = function () {
@@ -18,12 +18,38 @@ if (postButton) {
   }
 }
 
-
 newPostModal.onclick = function (event) {
   if (event.target == newPostModal) {
     newPostModal.style.display = "none"
   }
 }
+
+var pfpButton = document.getElementById("pfp-button")
+var pfpModal = document.getElementById("new-profile-pic-modal")
+var pfpImageSrc = document.getElementById("pf-image-src")
+var pfpUploadedImage = document.getElementById("pf-image-uploaded")
+pfpButton.onclick = function () {
+  if (pfpModal.style.display != "grid") {
+    pfpModal.style.display = "grid"
+  }
+}
+
+pfpModal.onclick = function (event) {
+  if (event.targe == pfpModal) {
+    pfpModal.style.display = "none"
+  }
+}
+
+pfpImageSrc.addEventListener("change", function () {
+  const files = pfpImageSrc.files
+  if (files.length == 0) {
+    pfpUploadedImage.src = ""
+    pfpUploadedImage.style.backgroundColor = "black"
+  } else {
+    const file = files[0]
+    pfpUploadedImage.style.backgroundImage = `url(${URL.createObjectURL(file)})`
+  }
+})
 
 imageSrc.addEventListener("change", function () {
   const files = imageSrc.files
@@ -37,23 +63,24 @@ imageSrc.addEventListener("change", function () {
 })
 
 document.getElementById("cp-return-url").value = window.location.href
+document.getElementById("pf-return-url").value = window.location.href
 
 // for follower/following modals
 
 followers.onclick = function () {
-  followModal.style.display = "block";
-  followerDiv.style.display = "block";
-  followDiv.style.display = "none";
+  followModal.style.display = "block"
+  followerDiv.style.display = "block"
+  followDiv.style.display = "none"
 }
 
 following.onclick = function () {
-  followModal.style.display = "block";
-  followerDiv.style.display = "none";
-  followDiv.style.display = "block";
+  followModal.style.display = "block"
+  followerDiv.style.display = "none"
+  followDiv.style.display = "block"
 }
 
 followModal.onclick = function (event) {
   if (event.target == followModal) {
-    followModal.style.display = "none";
+    followModal.style.display = "none"
   }
 }
